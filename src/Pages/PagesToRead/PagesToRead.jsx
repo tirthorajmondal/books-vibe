63edd5310ac91019bcbbbffb94a43e161e74880a
tirthorajmondal <tirtho.raj.2003@gmail.com>
2024-09-08 14:37:33 +0600

    chart added but some error not fixed

--- src/Pages/PagesToRead/PagesToRead.jsx
+++ src/Pages/PagesToRead/PagesToRead.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import { ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip, BarChart, Bar, Cell, } from 'recharts';
 
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'purple'];
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+};
+
+const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
 const PagesToRead = () => {
+
+    const dataArray = JSON.parse(localStorage.getItem('read')) || [];
+    console.log(dataArray);
+
     return (
         <div className='min-h-screen'>
-           <h1>Graph for read books</h1>
+            <div className="">
+                <ResponsiveContainer width="100%" aspect={2.5} >
+                    <BarChart data={dataArray}  >
+                        <CartesianGrid strokeDasharray='3 3' />
+                        <XAxis dataKey="bookName" interval={'preserveStartEnd'} tick={{ stroke: '#303eff70', width: '200', fontSize: '12', }}  tickSize='' />
+                        <YAxis />
+                        <Tooltip />
+                        <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {dataArray.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
